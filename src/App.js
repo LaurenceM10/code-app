d95968ed2a893883d155150bdb4c7404ea6dec11
Lauren <laurenmontenegro10@gmail.com>
2018-05-22 22:12:02 -0600

    Fetching repos from github

--- src/App.js
+++ src/App.js
@@ -1,21 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import logo from './logo.svg'
 import './App.css';
 
+import gql from 'graphql-tag';
+import {graphql} from 'react-apollo';
+
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        console.log(this.props);
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <div className="App-intro">
+                    {
+                        this.props.data.loading === true
+                            ? "Loading"
+                            : this.props.data.search.edges.map(data =>
+                                <ul key={data.node.id}>
+                                    <li style={{fontWeight: 'bold'}}>
+                                        <a href={data.node.url}>{data.node.name}</a>
+                                    </li>
+                                    <li>{data.node.description}</li>
+                                </ul>
+                            )
+                    }
+                </div>
+            </div>
+        );
+    }
 }
 
-export default App;
+
+// language=GraphQL
+const repoQuery = gql`
+    query ($name: String!) {
+        search(query: $name, last: 10, type: REPOSITORY) {
+            edges {
+                node {
+                    ... on Repository {
+                        id
+                        name
+                        description
+                        url
+                    }
+                }
+            }
+        }
+    }
+`;
+
+const AppWithData = graphql(
+    repoQuery,
+    {
+        options: {
+            variables: {
+                name: "react"
+            }
+        }
+    }
+)(App);
+
+export default AppWithData;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {ApolloProvider} from 'react-apollo';
+import {ApolloClient} from 'apollo-client';
+import {HttpLink} from 'apollo-link-http';
+import {InMemoryCache} from 'apollo-cache-inmemory';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// GitHub Access Token
+const token = "";
+
+// GitHub endpoint
+const httpLink = {
+    uri: 'https://api.github.com/graphql',
+    headers: {
+        authorization: `Bearer ${token}`
+    }
+};
+
+const client = new ApolloClient({
+    link: new HttpLink(httpLink),
+    cache: new InMemoryCache()
+});
+
+const Root = () => (
+    <ApolloProvider client={client}>
+        <App/>
+    </ApolloProvider>
+);
+
+ReactDOM.render(<Root/>, document.getElementById('root'));
 registerServiceWorker();
